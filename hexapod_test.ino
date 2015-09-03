5d6d8ca0c4312f73f5fafada0e804b7070087882
heyyeh12 <heyyeh12@aol.com>
2015-09-03 08:12:22 -0500

    wrapped test code blocks in functions

--- hexapod_test.ino
+++ hexapod_test.ino
@@ -6,7 +6,6 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(); // default address (0x4
 /* Pulse width min and max */
 #define SERVOMIN  128 // this is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX  551 // this is the 'maximum' pulse length count (out of 4096)
-#define SERVODIF  427 // difference between two (for reference)
 #define FREQ      60  // runtime frequency
 
 /*        FRONT
@@ -57,12 +56,16 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(); // default address (0x4
 
 double sweepTime;
 
-/* FUNCTION PROTOTYPES (so order doesn't matter) */
+/* FUNCTION PROTOTYPES (so order of function definitions doesn't matter) */
 void sweepServo(uint8_t n, uint16_t start_ang, uint16_t end_ang, double t, int wait = 0);
 void setServo(uint8_t n, uint16_t deg, int wait = 0);
 void setServos(uint8_t servos[], uint16_t poses[], int n, int wait = 0);
-void oneLegWalk(uint8_t hip, uint8_t knee, uint16_t forward, uint16_t backward, uint16_t up, uint16_t down, uint8_t wait = 1000);
 void initializeServos(int servos, uint16_t angle);
+
+void circleLegWalk();
+void Twist();
+void oneLegWalk(uint8_t hip, uint8_t knee, uint16_t forward, uint16_t backward, uint16_t up, uint16_t down, uint8_t wait = 1000);
+void legWalk();
 
 /* SETUP LOOP */
 void setup() {
@@ -71,142 +74,70 @@ void setup() {
   pwm.begin();
   pwm.setPWMFreq(FREQ);  // Analog servos run at ~60 Hz updates
   /* Initial position */
-//  setServo(0, 90);
-//  setServo(1, 90);
-//  setServo(2, 90);
-//  setServo(3, 90);
-//  setServo(4, 90);
-//  setServo(5, 90);
-//  setServo(6, 90);
-//  setServo(7, 90);
-//  setServo(8, 90);
-//  setServo(9, 90);
-//  setServo(10, 90);
-//  setServo(11, 90);
-//  setServo(12, 90);
-//  setServo(13, 90);
-//  setServo(14, 90);
-//  setServo(15, 90);
-
- sweepTime = 1;
+  // setAllServos(90);
 }
 
+/*************
+ * MAIN LOOP *
+ *************/
 void loop() {
 
-//uint8_t hips[] = {HIP1, HIP2, HIP3, HIP4, HIP5, HIP6};
-//sweepServos(hips, FORWARD1, BACKWARD1, 1, 6, 2000);
-//sweepServos(hips, BACKWARD1,FORWARD1, 2, 6, 2000);
-
-
-/* TWIST */
-//sweepServo(HIP1, FORWARD1, BACKWARD1, 2);
-//sweepServo(HIP2, FORWARD2, BACKWARD2, 2);
-//sweepServo(HIP3, FORWARD3, BACKWARD3, 2);
-//
-//sweepServo(HIP4, FORWARD4, BACKWARD4, 2);
-//sweepServo(HIP5, FORWARD5, BACKWARD5, 2);
-//sweepServo(HIP6, FORWARD6, BACKWARD6, 2);
-//
-//delay(3000);
-//
-//sweepServo(HIP1, BACKWARD1, FORWARD1, 2);
-//sweepServo(HIP2, BACKWARD2, FORWARD2, 2);
-//sweepServo(HIP3, BACKWARD3, FORWARD3, 2);
-//
-//sweepServo(HIP4, BACKWARD4, FORWARD4, 2);
-//sweepServo(HIP5, BACKWARD5, FORWARD5, 2);
-//sweepServo(HIP6, BACKWARD6, FORWARD6, 2);
-//
-//delay(3000);
+/*Set to 90*/
+setAllServos(90);
 /********************/
 
 /* CIRCLE LEG WALK */
-//oneLegWalk(HIP1, KNEE1, FORWARD1, BACKWARD1, UP1, DOWN1);
-//oneLegWalk(HIP2, KNEE2, FORWARD2, BACKWARD2, UP2, DOWN2);
-//oneLegWalk(HIP3, KNEE3, FORWARD3, BACKWARD3, UP3, DOWN3);
-//oneLegWalk(HIP4, KNEE4, BACKWARD4, FORWARD4, UP4, DOWN4);
-//oneLegWalk(HIP5, KNEE5, BACKWARD5, FORWARD5, UP5, DOWN5);
-//oneLegWalk(HIP6, KNEE6, BACKWARD6, FORWARD6, UP6, DOWN6);
+//circleLegWalk();
 /********************/  
 
-//  setServo(0, 0);
-//  setServo(1, 0);
-//  setServo(2, 0);
-//  setServo(3, 0);
-//  setServo(6, 0);
-//  setServo(7, 0);
-//  setServo(4, 0);
-//  setServo(5, 0);
-//  setServo(8, 0);
-//  setServo(9, 0);
-//  setServo(10, 0);
-//  setServo(11, 0);
-//  setServo(12, 0);
-//  setServo(13, 0);
-//  setServo(14, 0);
-//  setServo(15, 0, 1000);
-
-/*Set to 90*/
-//  setServo(0, 90);
-//  setServo(1, 90);
-//  setServo(2, 90);
-//  setServo(3, 90);  
-//  setServo(6, 90);
-//  setServo(7, 90);
-//  setServo(4, 90);
-//  setServo(5, 90);
-//  setServo(8, 90);
-//  setServo(9, 90);
-//  setServo(10, 90);
-//  setServo(11, 90);
-//  setServo(12, 90);
-//  setServo(13, 90);
-//  setServo(14, 90);
-//  setServo(15, 90, 3000);
-/************/ 
-//  setServo(0, 110);
-//  setServo(1, 110);
-//  setServo(2, 110);
-//  setServo(3, 110);  
-//  setServo(6, 110);
-//  setServo(7, 110);
-//  setServo(4, 110);
-//  setServo(5, 110);
-//  setServo(8, 110);
-//  setServo(9, 110);
-//  setServo(10, 110);
-//  setServo(11, 110);
-//  setServo(12, 110);
-//  setServo(13, 110);
-//  setServo(14, 110);
-//  setServo(15, 110, 3000);
+/* TWIST */
+//Twist();
+/********************/
   
 /* LEG WALK */
-/* 1) sweep back */
-//  setServo(HIP1, BACKWARD1);
-//  setServo(HIP3, BACKWARD3);
-//  setServo(HIP5, BACKWARD5);
-//  delay(1000);
-/* 2) leg up */
-//  setServo(KNEE1, UP1);
-//  setServo(KNEE3, UP3);
-//  setServo(KNEE5, UP5);
-//  delay(1000);
-/* 3) sweep forward */
-//  setServo(HIP1, FORWARD1);
-//  setServo(HIP3, FORWARD3);
-//  setServo(HIP5, FORWARD5);
-//  delay(1000);
-/* 4) leg down */
-//  setServo(KNEE1, DOWN1);
-//  setServo(KNEE3, DOWN3);
-//  setServo(KNEE5, DOWN5);
-//  delay(1000);
-/************/
+// legWalk();
+// oneLegWalk(HIP1, KNEE1, FORWARD1, BACKWARD1, UP1, DOWN1, 1000);
+/*******************/
+
 }
+
 /*****************
  * "DANCE" MOVES *
  *****************/
+void circleLegWalk() {
+  oneLegWalk(HIP1, KNEE1, FORWARD1, BACKWARD1, UP1, DOWN1);
+  oneLegWalk(HIP2, KNEE2, FORWARD2, BACKWARD2, UP2, DOWN2);
+  oneLegWalk(HIP3, KNEE3, FORWARD3, BACKWARD3, UP3, DOWN3);
+  oneLegWalk(HIP4, KNEE4, BACKWARD4, FORWARD4, UP4, DOWN4);
+  oneLegWalk(HIP5, KNEE5, BACKWARD5, FORWARD5, UP5, DOWN5);
+  oneLegWalk(HIP6, KNEE6, BACKWARD6, FORWARD6, UP6, DOWN6);
+}
+ 
+/* Twist middle with stationary legs */
+void Twist() {
+  sweepServo(HIP1, FORWARD1, BACKWARD1, 2);
+  sweepServo(HIP2, FORWARD2, BACKWARD2, 2);
+  sweepServo(HIP3, FORWARD3, BACKWARD3, 2);
+  
+  sweepServo(HIP4, BACKWARD4, FORWARD4, 2);
+  sweepServo(HIP5, BACKWARD5, FORWARD5, 2);
+  sweepServo(HIP6, BACKWARD6, FORWARD6, 2);
+  
+  delay(3000);
+  
+  sweepServo(HIP1, BACKWARD1, FORWARD1, 2);
+  sweepServo(HIP2, BACKWARD2, FORWARD2, 2);
+  sweepServo(HIP3, BACKWARD3, FORWARD3, 2);
+
+  sweepServo(HIP4, FORWARD4, BACKWARD4, 2);
+  sweepServo(HIP5, FORWARD5, BACKWARD5, 2);
+  sweepServo(HIP6, FORWARD6, BACKWARD6, 2);
+  
+
+  
+  delay(3000);
+}
+ 
 /* Untested: Walk one leg */
 void oneLegWalk(uint8_t hip, uint8_t knee, uint16_t forward, uint16_t backward, uint16_t up, uint16_t down, uint8_t wait)
 {
@@ -224,6 +155,31 @@ void oneLegWalk(uint8_t hip, uint8_t knee, uint16_t forward, uint16_t backward, 
   delay(wait); 
 }
 
+/* Uncomment/modify below to specify which leg */
+void legWalk()
+{
+  /* 1) sweep back */
+  setServo(HIP1, BACKWARD1);
+//  setServo(HIP3, BACKWARD3);
+//  setServo(HIP5, BACKWARD5);
+//  delay(1000);
+/* 2) leg up */
+  setServo(KNEE1, UP1);
+//  setServo(KNEE3, UP3);
+//  setServo(KNEE5, UP5);
+//  delay(1000);
+/* 3) sweep forward */
+  setServo(HIP1, FORWARD1);
+//  setServo(HIP3, FORWARD3);
+//  setServo(HIP5, FORWARD5);
+//  delay(1000);
+/* 4) leg down */
+  setServo(KNEE1, DOWN1);
+//  setServo(KNEE3, DOWN3);
+//  setServo(KNEE5, DOWN5);
+//  delay(1000);
+}
+
 /********************
  * USEFUL FUNCTIONS *
  ********************/
@@ -237,7 +193,7 @@ void setServo(uint8_t n, uint16_t deg, int wait)
   delay(wait);
 }
 
-/* Useful to initialize X number of servos to angle */
+/* Set all servos to angle (in degrees) */
 void setAllServos(uint16_t angle)
 {
   for (int i = 0; i < 15; i++) {
@@ -245,7 +201,7 @@ void setAllServos(uint16_t angle)
   }
 }
 
-/* Sweep from start_ang to end_ang in t seconds, with optional wait (in milliseconds) */
+/* Untested: Sweep from start_ang to end_ang in t seconds, with optional wait (in milliseconds) */
 void sweepServo(uint8_t n, uint16_t start_ang, uint16_t end_ang, double t, int wait)
 {
    uint16_t start_pulse = map(start_ang, 0, 180, SERVOMIN, SERVOMAX);
